0607c3f13c76d0210edd23a01df2b5f709c4dde4
2907 <82876040+ChanJeunlam@users.noreply.github.com>
2025-05-07 01:03:07 +0000

    Add GitHub Actions deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'ocean-login';
 
 const nextConfig = {
   output: 'export',
-  basePath: '/ocean-login', // 替换为您的 GitHub 仓库名称
-  assetPrefix: isProd ? '/ocean-login/' : '',
+  distDir: 'dist',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}` : '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
